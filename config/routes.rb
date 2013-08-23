546b8c9ef7cbe866b65b605fe10185f759ba47e8
Ben Luck <benjamonstyles@gmail.com>
2013-08-23 20:48:50 +0200

    test commit heroku

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ SuckerPunched::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#GappedAnimation'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
