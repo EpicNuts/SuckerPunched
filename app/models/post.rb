64e35b7175b07fb13c5c10caf3d2ae3c5636a82e
Ben Luck <benjamonstyles@gmail.com>
2013-08-24 19:21:54 +0200

    Catch Up

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessible :author_id, :body, :category_id, :title
+  attr_accessible :title, :body, :author_id, :category_id
+  belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SuckerPunched::Application.routes.draw do
   get "home/index"
     resources :posts
+    resources :categories
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
