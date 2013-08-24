9314f69d82555d369ad3fac8680ca449e051c03d
Ben Luck <benjamonstyles@gmail.com>
2013-08-24 15:08:56 +0200

    heroku update rake

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'This is some sample text for the SuckerPunched Webapp';
+		@content_second = 'This is some more sample text';
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+			
+	def destroy
+
+	end
+end
