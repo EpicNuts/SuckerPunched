a39f3a6bd13769618978a6faa999173fae08c6f4
EpicNuts <benjamonstyles@gmail.com>
2013-09-26 14:56:16 +0200

    swapped

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,15 +1,4 @@
 ActiveAdmin::Dashboards.build do
-    section 'Categories',:priority => 2 do
-        table_for Category.order("id desc").limit(15) do
-            column :id
-            column "Category Title",:name do |category|
-                link_to category.name,[:admin,category]
-            end
-            column :created_at
-        end
-        strong (link_to "Show All Categories",:admin_categories)
-    end
-
     section 'Recent Posts',:priority => 1 do
         table_for Post.order("id desc").limit(15) do
             column :id
@@ -23,5 +12,14 @@ ActiveAdmin::Dashboards.build do
         strong (link_to "Show All Posts",:admin_posts)
     end
 
-    
+    section 'Categories',:priority => 2 do
+        table_for Category.order("id desc").limit(15) do
+            column :id
+            column "Category Title",:name do |category|
+                link_to category.name,[:admin,category]
+            end
+            column :created_at
+        end
+        strong (link_to "Show All Categories",:admin_categories)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(:version => 20130925135852) do
     t.string   "title"
     t.text     "body"
     t.integer  "category_id"
-    t.integer  "author_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer  "admin_user_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
