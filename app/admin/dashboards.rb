1bff7d85ea30388d7a4bb678437e996c17c74ab5
EpicNuts <benjamonstyles@gmail.com>
2013-09-29 15:10:07 +0200

    Dashboard fix

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,5 +1,44 @@
-ActiveAdmin::Dashboards.build do
-    section 'Recent Posts',:priority => 1 do
+ActiveAdmin.register_page "Dashboard" do
+
+  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    div :class => "blank_slate_container", :id => "dashboard_default_message" do
+      span :class => "blank_slate" do
+        span I18n.t("active_admin.dashboard_welcome.welcome")
+        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      end
+    end
+
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+    # columns do
+    #   column do
+    #     panel "Recent Posts" do
+    #       ul do
+    #         Post.recent(5).map do |post|
+    #           li link_to(post.title, admin_post_path(post))
+    #         end
+    #       end
+    #     end
+    #   end
+
+    #   column do
+    #     panel "Info" do
+    #       para "Welcome to ActiveAdmin."
+    #     end
+    #   end
+    # end
+  end # content
+end
+
+=begin
+    
+rescue Exception => e
+    
+end
+ActiveAdmin.register_page "Dashboard" do
+    menu 'Recent Posts',:priority => 1 do
         table_for Post.order("id desc").limit(15) do
             column :id
             column "Post Title",:title do |post|
@@ -23,3 +62,4 @@ ActiveAdmin::Dashboards.build do
         strong (link_to "Show All Categories",:admin_categories)
     end
 end
+=end
