5d5c09e627c3f06571e460c2d9375d26d1799702
EpicNuts <benjamonstyles@gmail.com>
2013-09-29 14:39:32 +0200

    fixed

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,6 +1,7 @@
 class PostComment < ActiveRecord::Base
   belongs_to :post
-  attr_accessible :body, :email, :name
+  attr_accessible :body, :email, :name, :post
+
 
   validates_presence_of :name
   validates_length_of :name, :within => 2..20
